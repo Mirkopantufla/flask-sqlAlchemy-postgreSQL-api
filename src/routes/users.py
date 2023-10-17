e46193a79a42a87775a0f71114c665080db3f54c
mikish_ash@hotmail.com <mikish_ash@hotmail.com>
2023-10-16 22:44:11 -0300

    Request de los datos en json para ser almacenados como nuevo usuario

--- src/routes/users.py
+++ src/routes/users.py
@@ -9,6 +9,14 @@ api = Blueprint('api_users', __name__)
 @api.route('/register', methods=['POST'])
 def user_register():
     
+    rut_number = request.json.get('rut_number')
+    first_name = request.json.get('first_name')
+    last_name = request.json.get('last_name')
+    phone_number = request.json.get('phone_number')
+    email = request.json.get('email')
+    password = request.json.get('password')
+    terms_conditions = request.json.get('terms_conditions')
+    register_date = request.json.get('register_date')
     users = User.query.all()
     users = list(map(lambda user: user.serialize(), users))
 
